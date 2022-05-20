387384b5c0a067cc154a83f7c69bed7dac79819b
ArtGorkaDeveloper <dev@artgorka.ru>
2022-05-20 17:02:44 +0300

    20.05.2022 17:02

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <Header />
   <router-view />
+  <Footer/>
 </template>
 <script>
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   components: {
     Header,
+    Footer
   },
   mounted() {
     
