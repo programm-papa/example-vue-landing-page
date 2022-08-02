cb8e2bfa42729e0c2a0c7a9e1deb21d75bf1e591
programm-papa <kpavel_wrk@mail.ru>
2022-08-02 17:03:55 +0300

    add y_metrika update footer logo

--- src/main.js
+++ src/main.js
@@ -3,5 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import VueYandexMetrika from 'vue-yandex-metrika'
 
-createApp(App).use(store).use(router).mount("#app");
+
+
+createApp(App).use(store).use(router).use(VueYandexMetrika, {
+    id: 89818994,
+    router: router,
+    env: "production"
+    // env: process.env.NODE_ENV
+    // other options
+}).mount("#app");
